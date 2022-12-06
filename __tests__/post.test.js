ac5dd84dba346703f8caca88a412fe55ae111159
Charlie Hengemihle <charliehengemihle@gmail.com>
2022-12-06 12:10:34 -0700

    sending posts and passing tests!

--- __tests__/post.test.js
+++ __tests__/post.test.js
@@ -2,6 +2,7 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+const users = require('../lib/controllers/users.js');
 
 jest.mock('../lib/services/github');
 
@@ -15,15 +16,16 @@ describe('posts', () => {
 
   it('POST /api/v1/posts creates a new post as current user', async () => {
     const agent = request.agent(app);
-    await agent.get('/api/v1/github/callback?code=42').redirects(1);
-    console.log(agent, 'POOOOO');
-    const newPost = { detail: 'Wow! Whatuhpost!' };
+    const user = await agent
+      .get('/api/v1/github/callback?code=42')
+      .redirects(1);
+    const newPost = { detail: 'Wow! Whatuhpost!', github_id: user.body.id };
     const res = await agent.post('/api/v1/posts').send(newPost);
-    // expect(res.status).toBe(200);
+    expect(res.status).toBe(200);
     expect(res.body).toEqual({
       id: expect.any(String),
       detail: newPost.detail,
-      github_id: agent.github_id,
+      github_id: newPost.github_id,
     });
   });
 });
